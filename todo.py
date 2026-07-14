5b28b24f5863992a7a45f2d69267215e583d8b5b
anagha <alxcity5@gmail.com>
2026-07-14 14:50:16 +0530

    Add main menu for to-do list

--- todo.py
+++ todo.py
@@ -1,1 +1,26 @@
-print("Welcome to the To-Do List!")
+tasks = []
+
+while True:
+    print("\n===== TO-DO LIST =====")
+    print("1. View Tasks")
+    print("2. Add Task")
+    print("3. Delete Task")
+    print("4. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        print("View Tasks selected")
+
+    elif choice == "2":
+        print("Add Task selected")
+
+    elif choice == "3":
+        print("Delete Task selected")
+
+    elif choice == "4":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid option.")
